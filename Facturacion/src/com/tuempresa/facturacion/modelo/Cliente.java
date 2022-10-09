62d57ea5a963afd89f1421b6a22a919465faa243
wilberthechazarreta <114036147+wilberthechazarreta@users.noreply.github.com>
2022-10-09 17:54:35 -0500

    leccion 5 - modelar completo

---
+++ Facturacion/src/com/tuempresa/facturacion/modelo/Cliente.java
@@ -0,0 +1,24 @@
+package com.tuempresa.facturacion.modelo;
+
+import javax.persistence.*;
+
+import org.openxava.annotations.*;
+
+import lombok.*;
+
+@Entity 
+@Getter @Setter
+@View(name = "Simple", members = "numero, nombre")
+public class Cliente {
+	
+	@Id
+	@Column(length = 6)
+	int numero;
+	
+	@Column(length = 50)
+	@Required
+	String nombre;
+	
+	@Embedded
+	Direccion direccion;
+}
